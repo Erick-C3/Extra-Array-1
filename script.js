819488ffe391652214a977cee99ece9498df5c62
Erick <erick.nahuelpaiva.001@gmail.com>
2022-08-05 17:02:23 -0300

    agregado script faltante

---
+++ script.js
@@ -0,0 +1,6 @@
+const imagenesProductos = [
+    "https://http2.mlstatic.com/D_NQ_NP_2X_619126-MLA50232132766_062022-F.webp",
+    "https://http2.mlstatic.com/D_NQ_NP_2X_847906-MLA49763193559_042022-F.webp",
+    "https://http2.mlstatic.com/D_NQ_NP_991401-MLA31351342525_072019-O.webp"
+];
+const nombreProductos = ["tv", "celular", "pc"];
